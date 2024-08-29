9739c1e4935e301586e6a390f176e9b698465225
mushtaq <75521992+goatmanking@users.noreply.github.com>
2024-08-29 22:02:50 +0500

    green red multiplayer square game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock-paper-scissors
+
 First Multiplayer game
+
+**P.S:** ngl I suck at this, I will complete the rock paper scissor game after I read about networking, threading and sockets more so I actually understand what is happening moving forward.
---
+++ server.py
@@ -0,0 +1,66 @@
+import socket
+from _thread import *
+import sys
+
+server = "ADD-YOUR-LOCAL-IP-ADDRESS-HERE"
+port = 5555
+
+
+if (server == "ADD-YOUR-LOCAL-IP-ADDRESS-HERE"):
+    print("Brother please insert your local ip address in server (you can find local ip address using ipconfig) and set the local ip address in network.py aswell, also you need to run server.py in 1 terminal and client.py in 2 seperate terminals to witness the greatness of this project!")
+    
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0),(100,100)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
+        try:
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
+                print("Sending : ", reply)
+
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
